b3b0c75aa93ff70f225ff9c81f912f639e7d21c9
Michael Banini <mikeis2salty@gmail.com>
2025-10-14 08:46:55 -0400

    This is a test

---
+++ DoNow.js
@@ -0,0 +1,15 @@
+function CalcRetangleArea(height,length){
+    let area=0;
+    area=height*length;
+    console.log(area)
+    }
+    
+
+    function CalcCircleArea(radius){
+        let area=0;
+        area=3.14*radius**2;
+        console.log(area)
+        }
+        
+(CalcRectangleArea(5, 10)); // 50
+(CalcuCircleArea(3)); // 28.27
